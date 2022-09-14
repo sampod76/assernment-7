1b217bba669ae234dd4e269a7ad1cbbf4e8a86cd
sampod76 <sampodnath76@gmail.com>
2022-09-15 01:21:00 +0600

    fixed time counter 2 decimal

--- script.js
+++ script.js
@@ -134,6 +134,8 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
+  const timeSpentTwo=parseFloat(timeSpent).toFixed(2)
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpentTwo : 0} seconds`;
 }, 1000);
