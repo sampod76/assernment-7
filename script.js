8bd78f94bc22d493ecda1f3891965936017493a7
sampod76 <sampodnath76@gmail.com>
2022-09-15 01:24:33 +0600

    fixed mistake dialogue box timer

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ const gameOver = () => {
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenTwoDacimel=parseFloat(timeTaken).toFixed(2)
 
   // show result modal
   resultModal.innerHTML = "";
@@ -80,7 +81,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTakenTwoDacimel}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
